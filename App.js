77afb5565e81ba7f07d153db65c140e1bb47dd4d
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-03-17 07:25:41 +0200

    remove fb folder

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from './src/components/layout/index';
 import Login from './src/components/login/index';
 import Register from './src/components/reg/';
-import Facebook from './src/components/fb/';
 import Chat from './src/components/chat/';
 
 
@@ -13,7 +12,6 @@ const UsersManager = StackNavigator({
     Home: { screen: HomeScreen },
     Login: { screen: Login },
     Register: {screen: Register},
-    Facebook: {screen: Facebook},
     Chat: {screen: Chat},
 })
 
